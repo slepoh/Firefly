cc354fed4495f287ca99e2c621579e97af717e3b
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-06 00:45:23 +0800

    Update src/config/announcementConfig.ts via FireflyCMS

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/announcementConfig";
 
 export const announcementConfig: AnnouncementConfig = {
 	// 公告标题
-	title: "公告",
+	title: "温馨提示",
 
 	// 公告内容
 	content: "全栈开发工程师 & 全网营销推广专家，拥有15年互联网产品开发与运营经验，专注于Web开发、移动应用、AI人工智能和数字营销解决方案。致力于为客户提供创新的技术实现和业务增长方案。",
@@ -14,7 +14,7 @@ export const announcementConfig: AnnouncementConfig = {
 		// 启用链接
 		enable: true,
 		// 链接文本
-		text: "了解更多",
+		text: "查看全部",
 		// 链接 URL
 		url: "/about/",
 		// 内部链接
