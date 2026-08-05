2752d51449aadd5c888310103b34ebc8726303af
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-05 14:50:20 +0800

    Update displaySettingsConfig.ts

--- src/config/displaySettingsConfig.ts
+++ src/config/displaySettingsConfig.ts
@@ -23,7 +23,7 @@ export const displaySettingsConfig: DisplaySettingsConfig = {
 	// ── 壁纸 (Wallpaper) ──────────────────────────────────
 
 	// 壁纸模式切换开关
-	wallpaperModeSwitchable: false,
+	wallpaperModeSwitchable: true,
 
 	// 水波纹动画开关
 	wavesSwitchable: true,
