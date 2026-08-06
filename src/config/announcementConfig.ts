465d82f48843be8048b23d3ffbae14c11072ae66
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-06 09:10:39 +0800

    Update announcementConfig.ts

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -8,7 +8,7 @@ export const announcementConfig: AnnouncementConfig = {
 	content: "全栈开发工程师 & 全网营销推广专家，拥有15年互联网产品开发与运营经验，专注于Web开发、移动应用、AI人工智能和数字营销解决方案。致力于为客户提供创新的技术实现和业务增长方案。",
 
 	// 是否允许用户关闭公告
-	closable: true,
+	closable: false,
 
 	link: {
 		// 启用链接
