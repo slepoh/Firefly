e0f98811e8c072b752de54dec566ae14a32f0bb8
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-06 21:01:13 +0800

    Update sidebarConfig.ts via FireflyCMS

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -243,7 +243,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：广告栏组件 3
 			type: "advertisement",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 组件位置
 			position: "sticky",
 			// 是否在文章详情页显示
