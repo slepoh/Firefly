705a20c903a0b668e5b85624ac54f2f7b1f9649a
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-07 19:41:31 +0800

    Update profileConfig.ts via FireflyCMS

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
         name: "运营程序猿",
 
         // 个人签名
-        bio: "会开发、懂",
+        bio: "会开发、懂运营、",
 
         // 链接配置
         // 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
