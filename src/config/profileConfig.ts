6c1f70fdab137b3b095163e7098eee080fbb99ff
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-07 19:40:57 +0800

    Update profileConfig.ts via FireflyCMS

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -24,7 +24,7 @@ export const profileConfig: ProfileConfig = {
                 {
                         name: "qq",
                         icon: "fa7-brands:qq",
-                        url: "https://qm.qq.com/q/ZGsFa8qX2G",
+                        url: "",
                         showName: false,
                 },
                 {
