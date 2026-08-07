3e9608d635f910b56915957eb21f57cde637ddc1
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-07 19:42:01 +0800

    Update profileConfig.ts via FireflyCMS

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -49,7 +49,7 @@ export const profileConfig: ProfileConfig = {
 
                   icon: "fa7-solid:envelope",
 
-                  url: "mailto:",
+                  url: "mailto:info@aioky.com，",
 
                   showName: false
 
