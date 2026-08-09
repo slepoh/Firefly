e6449cce2d5dd861efe5cf60cbe07928d1f4e661
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-08-09 23:23:38 +0800

    Update sidebarConfig.ts BY YiQiCMS

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -342,7 +342,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 是否启用该组件
 			enable: false,
 			// 是否在文章详情页显示
-			showOnPostPage: true,
+			showOnPostPage: false,
 			// 组件专属配置
 			specificConfig: {
 				siteInfo: {
